f68be887a2d111d78c4387e0c4eaedddf0b86ff2
anuchit.prasertsang@gmail.com <anuchit.prasertsang@gmail.com@b3b54f03-73a1-2c7b-07a8-02c5d639e876>
2013-11-06 07:20:42 +0000

    20131106:14.20::Start new sprint 2 4.6 and 4.7

---
+++ WebContent/js/app/SVD006/SVD006.js
@@ -0,0 +1,45 @@
+var SVD006 = {};
+
+Ext.onReady(function() {SVD006.resumeForm = new Ext.form.FormPanel({
+				applyTo : "content",
+				layout : 'column',
+				border : false,
+				width : 840,
+				style : {
+					"margin-left" : "auto",
+					"margin-right" : "auto",
+					"margin-top" : "50px"
+				},
+				defaults : {
+					xtype : 'container',
+					layout : 'form',
+					columnWidth : 1,
+					// labelWidth : 0,
+					anchor : '100%',
+					// hideBorders : true
+				},
+
+				items : [
+				   {
+					columnWidth : 0.5,
+					items : SVD006C.createButtonSubmit,
+
+					style : {
+						"margin-left" : "150px",
+
+					},
+
+				}, {
+					columnWidth : 0.5,
+					items : SVD006C.createButtonBack,
+					style : {
+						"margin-left" : "5px",
+
+					},
+
+				},{
+					items : SVD006C.tabPanel
+				} ],
+
+			});
+		});
